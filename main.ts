1c0eec1fe6d3cced855f89d9bd4b5f90fe9bbbf6
Fabio Nicoliello <32755862+fabionicoliellofab@users.noreply.github.com>
2025-07-02 14:59:53 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+radio.onReceivedValue(function (name, value) {
+    if (name == "Presion") {
+        radio.sendString("Presion")
+        radio.sendNumber(value)
+    } else {
+        if (name == "Temp") {
+            radio.sendString("Temp")
+            radio.sendNumber(value)
+        } else {
+            if (name == "Humedad") {
+                radio.sendString("Humedad")
+                radio.sendNumber(value)
+            }
+        }
+    }
+})
+radio.setTransmitPower(7)
+radio.setGroup(1)
