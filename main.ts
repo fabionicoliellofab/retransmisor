ffba7da2cbe4308d57a760d3dd816dc1faffdc5d
Fabio Nicoliello <32755862+fabionicoliellofab@users.noreply.github.com>
2025-07-02 16:49:14 -0300

    cambia el si

--- main.ts
+++ main.ts
@@ -1,20 +1,15 @@
 radio.onReceivedValue(function (name, value) {
-    OLED.writeStringNewLine("RECIBIENDO")
     OLED.writeStringNewLine(name)
     OLED.writeNumNewLine(value)
+    basic.pause(2000)
     if (name == "Presion") {
-        radio.sendString(name)
-        radio.sendNumber(value)
-    } else {
-        if (name == "Temp") {
-            radio.sendString(name)
-            radio.sendNumber(value)
-        } else {
-            if (name == "Humedad") {
-                radio.sendString(name)
-                radio.sendNumber(value)
-            }
-        }
+        radio.sendValue("Presion", value)
+    }
+    if (name == "Temp") {
+        radio.sendValue("Temp", value)
+    }
+    if (name == "Humedad") {
+        radio.sendValue("Humedad", value)
     }
 })
 radio.setTransmitPower(7)
